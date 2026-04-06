10260efcb4d19b97f745685bfd26502a33b912a4
Ace <132539186+Smart-Ace-Designs@users.noreply.github.com>
2026-04-05 23:57:01 -0500

    Remove empty ignorePatterns from config

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "oxfmt";
 
 export default defineConfig({
-  ignorePatterns: [],
   sortTailwindcss: {
     stylesheet: "./app/assets/css/main.css",
     functions: ["clsx", "cn"],
